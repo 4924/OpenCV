93a766362455ed1525c2b29ba2ca902f41000151
Elias Basler <e.e.basler@gmail.com>
2016-10-25 22:41:29 -0400

    another cool idea that doesn't work

--- Robticscv.py
+++ Robticscv.py
@@ -29,17 +29,26 @@ for s in os.listdir("files"):
 
     #print s
 
+    #def callback(x):
+    #    pass
+
+
     im = cv.resize(im, (320,240))
     #im = cv.bitwise_not(im)
 
-    im2 = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+    #im2 = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
     #cv.imshow('gray', im2)
 
-    th, im3 = cv.threshold(im2, 50, 255, cv.THRESH_TOZERO);
-    #im3 = cv.inRange(im2, np.array([160, 0, 0]), np.array([300, 100, 100]))
+    #th, im3 = cv.threshold(im2, 50, 255, cv.THRESH_TOZERO);
+    im4 = cv.cvtColor(im, cv.COLOR_BGR2HSV)
+    im6 = cv.inRange(im4, np.array([79, 162, 50]), np.array([98, 255, 255]))
+    ##im6 = cv.cvtColor(im5, cv.COLOR_HSV2BGR)
     #cv.imshow('threshold', im3)
+    #cv.imshow('color', im)
+    #cv.imshow('inRange', im6)
 
-    contours, hierarchy = cv.findContours(im3,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+
+    contours, hierarchy = cv.findContours(im6,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
 
 
     lan = [0]*5
@@ -54,29 +63,68 @@ for s in os.listdir("files"):
                 break
 
 
-    #(x,y),(MA,ma),angle = cv.fitEllipse(thing)
-    #print angle
-    for i in range(0, 3):
-        x,y,w,h = cv.boundingRect(thing[0])
-        crop_im = im[y:y+h, x:x+w]
-        cv.imwrite("file_output_new/"+str(i)+"|"+s, crop_im)
-    #cv.imshow('crop', crop_im)
+#(x,y),(MA,ma),angle = cv.fitEllipse(thing)
+#print angle
+#for i in range(0, 3):
+#    x,y,w,h = cv.boundingRect(thing[0])
+#    crop_im = im[y:y+h, x:x+w]
+#    cv.imwrite("file_output_new/"+str(i)+"|"+s, crop_im)
+#cv.imshow('crop', crop_im)
 
 
-    #save = thing
-    #saveHandler = open('example.contour', 'w')
-    #pickle.dump(save, saveHandler)
+#save = thing
+#saveHandler = open('example.contour', 'w')
+#pickle.dump(save, saveHandler)
 
-    #saveHandler = open('example.contour', 'r')
-    #savedThing = pickle.load(saveHandler)
-    #print cv.matchShapes(thing, savedThing, 1, 0.0)
+#saveHandler = open('example.contour', 'r')
+#savedThing = pickle.load(saveHandler)
+#print cv.matchShapes(thing, savedThing, 1, 0.0)
 
-    #print cv.contourArea(cv.convexHull(thing))
-    #print cv.contourArea(cv.convexHull(thing))/cv.contourArea(thing)*50
+#print cv.contourArea(cv.convexHull(thing))
+#print cv.contourArea(cv.convexHull(thing))/cv.contourArea(thing)*50
 
-    #im4 = cv.cvtColor(im3, cv.COLOR_GRAY2BGR)
-    #cv.drawContours(im4, thing[0:4], -1, (0,255,0), 1)
-    #cv.imwrite("realfilesas/"+s, im4)
-    #cv.imshow('title', im4)
-    #cv.waitKey(0)
-    #cv.destroyAllWindows()
+    im4 = cv.cvtColor(im6, cv.COLOR_GRAY2BGR)
+    cv.drawContours(im4, thing[0:3], -1, (0,255,0), 1)
+    cv.imwrite("filesas/"+s, im4)
+#cv.imshow('title', im4)
+#cv.waitKey(0)
+#cv.destroyAllWindows()
+#
+#cv.namedWindow('thresh')
+#
+## Track bars
+#cv.createTrackbar('lH','thresh', 0, 180, callback)
+#cv.createTrackbar('lS', 'thresh', 0, 255, callback)
+#cv.createTrackbar('lV', 'thresh', 0, 255, callback)
+#cv.createTrackbar('uH', 'thresh', 0, 180, callback)
+#cv.createTrackbar('uS', 'thresh', 0, 255, callback)
+#cv.createTrackbar('uV', 'thresh', 0, 255, callback)
+#
+## capture video
+#
+#
+#while True:
+#    # Getting values from track bars
+#    lH = cv.getTrackbarPos('lH', 'thresh')
+#    uH = cv.getTrackbarPos('uH', 'thresh')
+#    lS = cv.getTrackbarPos('lS', 'thresh')
+#    uS = cv.getTrackbarPos('uS', 'thresh')
+#    lV = cv.getTrackbarPos('lV', 'thresh')
+#    uV = cv.getTrackbarPos('uV', 'thresh')
+#
+#
+#    lowerb = np.array([lH, lS, lV], np.uint8)
+#    upperb = np.array([uH, uS, uV], np.uint8)
+#
+#    #print lowerb
+#    #print upperb
+#
+#    frame = cv.inRange(im4, lowerb, upperb)
+#    cv.imshow('thresh', frame)
+#
+#    if cv.waitKey(1) & 0xFF == ord('q'):
+#        break
+#
+#cap.release()
+#cv.destroyAllWindows()
+#
