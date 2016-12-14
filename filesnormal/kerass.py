14786bad91f12d0a8e553440da0dcc20127efed5
Elias Basler <e.e.basler@gmail.com>
2016-12-14 12:38:14 +0000

    more tests

--- filesnormal/kerass.py
+++ filesnormal/kerass.py
@@ -4,23 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
-from keras.datasets import mnist
 from keras.preprocessing.image import ImageDataGenerator
-
-#(X_train, y_train), (X_test, y_test) = mnist.load_data()
-#
-#
-#X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)
-#X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)
-#X_train = X_train.astype('float32')
-#X_test = X_test.astype('float32')
-#X_train /= 255
-#X_test /= 255
-#
-#
-#Y_train = np_utils.to_categorical(y_train, 10)
-#Y_test = np_utils.to_categorical(y_test, 10)
-
 
 model = Sequential()
 
@@ -45,39 +29,30 @@ train_datagen = ImageDataGenerator(
         zoom_range=0.2,
         horizontal_flip=True)
 
-# this is the augmentation configuration we will use for testing:
-# only rescaling
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-# this is a generator that will read pictures found in
-# subfolers of 'data/train', and indefinitely generate
-# batches of augmented image data
 train_generator = train_datagen.flow_from_directory(
-        'train',  # this is the target directory
-        target_size=(32, 32),  # all images will be resized to 150x150
+        'train',
+        target_size=(32, 32),
         batch_size=32,
-        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
 
-# this is a similar generator, for validation data
 validation_generator = test_datagen.flow_from_directory(
         'test',
         target_size=(32, 32),
         batch_size=32,
         class_mode='binary')
 
-#model.fit_generator(
-#        train_generator,
-#        samples_per_epoch=1064,
-#        nb_epoch=10,
-#        validation_data=validation_generator,
-#        nb_val_samples=106,
-#        verbose=1)
-#model.save_weights('first_try.h5')  # always save your weights after training or during training
-#///////////////////////////////////////////////////////////////////
-#model.fit(X_train, Y_train,
-#          batch_size=32, nb_epoch=10, verbose=1)
+model.fit_generator(
+        train_generator,
+        samples_per_epoch=1064,
+        nb_epoch=10,
+        validation_data=validation_generator,
+        nb_val_samples=106,
+        verbose=1)
+model.save_weights('try3.h5')  
 
-model.load_weights('first_try.h5')
+#model.load_weights('first_try.h5')
 score = model.evaluate_generator(validation_generator, val_samples=106)
 print score
 print model.metrics_names
